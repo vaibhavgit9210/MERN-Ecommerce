635ec6be2ffec435fb563ec4504f8348e0ffd98f
Vaibhav Kumar <vaibhavpro9210@gmail.com>
2024-07-11 22:49:11 +0530

    Added dotenv & gitignore file to make it more secure

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-import express from 'express';
-import colors from 'colors';
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
 
+//configure dotenv
+dotenv.config();
+
+//rest object
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Welcome to jungle</h1>');
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to jungle</h1>");
 });
 
-const PORT = 8080;
+//PORT
+const PORT = process.env.PORT || 8080;
 
+//run listen
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`.bgGreen.white);
+  console.log(`Server running in ${process.env.SERVER_MODE} on port ${PORT}`.bgGreen.white);
 });
