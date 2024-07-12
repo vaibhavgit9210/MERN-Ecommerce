8e3965fe9336c90cef69bb02835b1e65ac00c6cb
Vaibhav Kumar <vaibhavpro9210@gmail.com>
2024-07-13 00:59:45 +0530

    feat: add user registration API

---
+++ controllers/authController.js
@@ -0,0 +1,56 @@
+import userModel from "../models/userModel.js";
+import { hashPassword } from "../helpers/authHelper.js";
+
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+    //validations
+    if (!name) {
+      return res.send({ error: "Name is Required" });
+    }
+    if (!email) {
+      return res.send({ error: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ error: "Password is Required" });
+    }
+    if (!phone) {
+      return res.send({ error: "Phone number is Required" });
+    }
+    if (!address) {
+      return res.send({ error: "Address is Required" });
+    }
+    //check user
+    const existingUser = await userModel.findOne({ email });
+    //existing user
+    if (existingUser) {
+      return res.status(200).send({
+        success: true,
+        message:
+          "Already registered with the same email address, please login or use a different email address.",
+      });
+    }
+    //register user
+    const hashedPassword = await hashPassword(password);
+    //save
+    const user = await new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hashedPassword,
+    }).save();
+    res.status(201).send({
+      success: true,
+      message: "User registered successfully.",
+      user,
+    });
+  } catch (error) {
+    console.log(`An error occurred: ${error}`);
+    res.status(500).send({
+      success: false,
+      message: "Error in registration",
+      error,
+    });
+  }
+};
